814306f5e8342fe558552c47fd1b9d5f58f2cf9e
Andrea7t <146023266+Northdelcaca@users.noreply.github.com>
2023-11-30 19:24:37 +0100

    Add files via upload

---
+++ candid naruto/public/js/transition_page.js
@@ -0,0 +1,128 @@
+content_page1 = [
+    `<h3><strong>Kyōfu Senju Uchiha </strong></h3><img src="public/img/Apparence.png" style="width: auto;height: 50%;" alt="">`,
+    `
+    <h3><strong>Naissance</strong></h3>
+        <p>Le jour de la naissance de Kyōfu fut un jours Heureux les deux clans les plus puissants de Konoah enfin reuni par un lien de sang ne pouvais etre qu'un jours aggraffant un sourire au visage des membres de clans</p> 
+    <h3><strong>Premiers Pas </strong></h3>
+        <p>Depuis sa plus tendre enfance Kyōfu s'entraine avec son pere...ce dernier etant un des hommes les plus fort du village ne pouvais que donner un des meilleurs enseignements a son fils! Mais , vu qu'il n'avait encore l'age de commencer les entrainements de combat il lui aprit comment ce comporter et l'art de la stratégie.A l'age de seulement 4 ans il a commencé a apprendre tout cela que ce soit du coter des Senju ou des Uchiha. </p> 
+        <button onclick="PageSuivante(1)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>14 Decembre XXX</strong></h3>
+    <p>Apres une longue discussion avec mon pere ce dernier m'a autoriser a aller residé durant quelques mois chez un ancien ninja l'ayant former lui meme...Parfait cela me permettera d'en aprendre plus sur le mettier et comments il developes leurs "jutsu".Demain dès l'aube je serais en route pour le Mont Anabashu ou ce dernier reside.</p>
+    <h3><strong>15 Decembre 6 heures XXX</strong></h3>
+    <p>
+    C'est donc l'Heure du depart le soleil est a peine levé c'est parfait j'ai dit au revoir a mon pere et ma mere etait encore endormis.Mais bon cela ne m'etonne plus avec elle ! Alors en route pour devenir le meilleur ninja de la terre ! 
+    </p>
+    <h3><strong>15 Decembre 14 heures XXX</strong></h3>
+    <p>
+    Je suis enfin arriver devant sa maison cela ressemble plus a une grange qu'a une maison mais bon il faut souffrir pour etre fort alors allons y !
+    <p>
+    <button onclick="PageSuivante(2)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>18 Fevrier XXX</strong></h3>
+    <p>
+    Cela Fait maintenant deux Mois que je m'entraine avec le Vieux crouton,et mes efforts payent.Apres avoir parler un peu plus avec le vieux il m'a expliquer les pouvoirs de mes deux clans respectifs il m'a parler du Sharingan que je devait herité de ma mere et du Bokuton de mon pere mais d'apres lui,quand j'obtiendrais les Sharingan ils ne seront pas pareil...
+    </p>
+    <h3><strong>19 juin XXX</strong></h3>
+    <p>
+    C'est la fin de mon entrainement,mais que le debut de mon aventure lors de mon retour au village mon pere etait en reunion et ma mere dans la maison de clan Uchiha... Super votres fils rentre et vous etes meme pas la pour le revoir bah Bravo !
+    </p>
+    <button onclick="PageSuivante(3)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>Entré a l'Academie</strong></h3>
+    <p>
+    Kyōfu est maintenant rentrer a l'academie Ninja pour enfin devenir genin a l'age de 13 ans ce dernier n'en n'avait que 10 pour l'instant...Arrivé a l'academie Kyōfu excelait dans tout les domaine.Ce dernier ne voulait pas parler avec les eleves de l'Academie non pas par mepris...quoi que oui tres clairement par mepris mais il parlait quand meme avec deux amis d'enfance avec qui il jouait dans le parc lors de sa jeunesse.Certaine personnes jalouses de sont statut essayais de le persecuter,mais tout cela en vain Kyōfu entendait a peine les critiques.
+    </p>
+    <h3><strong>Altercation</strong></h3>
+    <p>
+    Lors d'une balade avec ses deux amis Kyōfu rencontra le groupe de personnes qui tentait de le persecuter a l'academie,quelle bonne occasion pour ces vielle crapule d'attaquer Kyōfu et ses amis sans ce faire prendre par les professeur alors les 8 petit garnement decidere de faire des boules de terre et de leurs lancer dessus...Biensur Kyōfu avec un grand ego ne pouvait pas laisser faire sa il a donc tout simplement foncer sur les 8 gamins avec pour objectif de leurs mettre une paté.
+    </p>
+    <button onclick="PageSuivante(4)">Page Précedente</button></div>
+    `,
+    `
+    <h3><strong>Les Années Passes</strong></h3>
+    <p>
+    Aujourd'hui c'etait le jours du passage au grade de genin en formation de Kyōfu biensur il etait major de sa promotion mais bon pour lui le plus important etait que sont pere soit present mais, comme d'habitute une mission l'attendait.Apres la ceremonie Kyōfu reçus une lettre de felicitation de son pere et ce dernier lui explique "On passe aux choses seruieuses maintenant fils" !
+    </p>
+    
+    <button onclick="PageSuivante(5)">Page Précedente</button></div>
+    `,
+    `
+
+    <h3><strong>Partie Charactere</strong></h3>
+    <p>
+    Kyōfu est un enfant assez reflechis et stratege dans ses conclusion mais ce dernier sait montrer quelques fois une sorte d'arrogance de part son sentiment du superiorité en vers les personnes qu'il meprise.Avec Kyōfu est le meilleurs des atout il est pret a mourir pour eux.
+    </p>
+    <h3><strong>Fin</strong></h3>
+    <p>
+    Merci d'avoir lu cette candidature je suis dispo sur discord pour des question en esperant etre prit bonne journée!
+    <p>
+    <button onclick="PageSuivante(6)">Page Précedente</button></div>
+    `,
+    `
+    <p>
+    les autres membres de l’escouade de tacticiens ne font qu’allouer des troupes a l’élimination de titans c’est stupide car c’est une expédition longue si on alloue nos troupe a l’élimination de titans dès le début on vas simplement épuisé nos troupe, c’est bien trop risqué, notre objectif doit être de trouvé un moyen d’éliminer les titans pas des les éliminer avec notre puissance militaire actuelle c’est impossible, mais impossible de les raisoner, la situation ne veut pas changer parceque 10 titans sont tomber on ne sais même pas comment il apparaisent par contre si 30 de nos hommes tombe la situation ne peux que changer, mais en tant que 1er C impossible de ce faire entendre. la situation m’énerve je souhaite que ma voix soit entendue au final, c’est comme lorsque j'étais à Utopia , c’est là que j’ai eu une idée je vais me battre rejoindre le commando en tant qu’aide temporaire le temps que la situation se calme, je remplacerais au moins un des leurs pour qu’il puissent parler de leur situation au tacticien, quelqu’un que j’ai connue et en qui j’ai confiance pour sa franchise et son honnêteté, mon niveau au tridimensionnel est certe médiocre mais si je doit faire ça pour être écouté alors je le ferais.
+    </p>
+
+    <button onclick="PageSuivante(7)">Page Précedente</button></div>
+    `,
+
+];
+content_page2 = [
+    `<h3><strong>Identité</strong></h3><br><p>Nom: Senju Uchihah </p><p>Prenom:Kyōfu </p><br><p>Lieux de Naissance: Konoha </p><p>Date de Naissance:  XXX</p><p>Age: 13 ans</p><br><p>Numéro D'identité: 4929 1049 6131 4085</p><p>Pere:Koyomi Senju</p><br><p>Lieux de Résidence: Konoha</p><br><div class="flex-box"><button onclick="PageSuivante(2)">Page Suivante</button></div>`,
+    `
+    <h3><strong>Premier coup XXX</strong></h3>
+    <p>A l'age de ses 5 ans Kyōfu commence l'entrainement au combat.Lors des premier entrainements ce dernier avait un grand mal a ce concentré mais,le Pere lui etait la pour le recentré...Sont niveau etait banal pour un enfant de 5ans mais, il excelait dans un art L'art du Kenjutsu son aisance a manier la lames etait meme effrayante pour un enfant de son age. </p>
+    <h3><strong>Cadeau !</strong></h3>
+    <p>
+    Aujourd'hui Kyōfu Faitait ses 6 ans pour son anniversaire ce dernier ne shouaitait pas de cadeau particulier a part un simple carnet une plume et de l'ancre...En effet apres avoir été former au base de l'ecriture ce dernier voulait ecrire tous ses entrainement et toutes ses journées</p>
+    <h3><strong>12 Décembre XXX</strong></h3>
+    <p>
+    Nous sommes le lendemain de mon anniversaire et je n'ai demandé que se cadeau en effet je trouve que les futilité que sont les cadeaux comme des nouveaux habit ou de simples jeux pour enfant de 6 ans ne m'aiderons pas dans la quete qu'est de surpasser mon pere,alors a partir d'Aujourd'hui je vait m'entrainer jouer et nuit afin de pouvoir y arriver.
+    </p>
+    <button onclick="PageSuivante(3)">Page Suivante</button></div>
+    `,
+    `
+    <h3><strong>16 Decembre XXX</strong></h3>
+    <p>
+    J'ai enfin fait la rencontre de ce vieux monsieur il ce nomme Kenta, j'avais deja vu des ninja veterant mais celui ci est l'incarnation meme d'un vieux crouton ramolie qui est trop resté dans la soupe.et en plus de cela il arrive tout de meme a raler! Mais bon on ressent tout de meme une sorte d'aura chez lui et cela m'intrigue et me force a le respecter.
+    </p>
+    <h3><strong>17 Decembre XXX</strong></h3>
+    <p>
+    IL EST COMPLETEMENT FOU! il m'a reveillé en pleine nuit pour que je m'entraine,m'a jeter des pierre dessus pour "Entrainer mes reflexes" Et m'a dit que la prochaine fois ce serait des kunai.Apres cela il m'a fait monter et descendre le Mont 10 fois desuite.
+    </p>
+    <button onclick="PageSuivante(4)">Page Suivante</button></div>
+    `,
+    `
+    <h3><strong>Il grandissent si vites</strong></h3>
+    <p>
+    il est l'heure pour moi le meilleur des naratteur de continuer a comter l'histoire de Kyōfu.Apres maintenant deux années passer ce dernier a Reussi a developper de nombreux ninjutsu et ce developer dans le KenJutsu.Sont pere lui etait de moin en moin present de part sont roles de chef des Ambu...Mais bon il avait tout de meme la presence de sa Mere.Et cela ne derangeais pas plus que ça Kyōfu car cela lui permettait d'acquerir un maximum d'information sur le sharingan et ses bien faits.
+    </p>
+    <button onclick="PageSuivante(5)">Page Suivante</button></div>
+    `,
+    `  
+    <h3><strong>Stop</strong></h3>
+    <p>Kyōfu n'eu meme pas le tant de levé la main qu'un ninja masqué ce teleportaire devant le jeunes Senju et le mit a terre pour le stopper.Les villains garnement apres avoir vu l'apparition d'un ninja des forces special prit la fuite.Le ninja masqué dit a Kyōfu "Arrette un peu tes betises tu veux si tu revien blessé imagine un peu la deception de ton pere! alors fait profil bas."juste apres ces paroles le ninja disparu,Kyōfu lui epoustoufler par la rapidité de l'homme resta bouchebé ses deux amis encores present dit "ça vas Kyōfu ?" ce dernier retorquere "Oui oui je dois rentrer on se voit demain les amis !"Apres les paroles du ninja masqué Kyōfu ce remit totalement en question "C'est vrai je ne peu pas me battre a tout vas comme ces sale racailles et si je les blesses qu'est ce qu'il arrivera a ma famille ? Allez stop Kyōfu maitenant tu te reprend en main jusqu'a ton arriver chez les genin</p>
+    <button onclick="PageSuivante(6)">Page Suivante</button></div>
+    `,
+    `
+    <h3><strong>Avenir !</strong></h3>
+    <p>
+    Seul l'avenir nous diras ce que deviendras Kyōfu ! Mais je pense qu'il iras loin tres loin et un jours peut etre reussiras sont nindo de surpasser son pere !
+    <button onclick="PageSuivante(7)">Page Suivante</button></div>
+    `,
+    `
+    `,
+];
+
+function PageSuivante(nb) {
+    document.getElementById("page1").innerHTML = content_page1[nb - 1];
+    document.getElementById("page2").innerHTML = content_page2[nb - 1];
+};
+
+setTimeout(function() {
+    PageSuivante(1);
+}, 10);
